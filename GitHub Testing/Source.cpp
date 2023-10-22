9ef25132932939a92bc5c749eadb68d0460008b5
canno <canno@LEGION>
2023-10-22 17:16:33 -0400

    Add project files.

---
+++ GitHub Testing/Source.cpp
@@ -0,0 +1,24 @@
+/*
+GitHub Testing
+Michael Cannon
+CIS 1202.501
+October 10, 2023
+
+This program displays the text "Hello, World!" to the console.
+It will be used to test code management.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+	
+	
+	//END
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
